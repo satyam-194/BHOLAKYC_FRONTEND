2d50436d836c10c9509239a7590770fd655abdcf
awanish kumar yadav <aay14155@gmail.com>
2026-04-19 21:00:38 +0530

    done

--- App.jsx
+++ App.jsx
@@ -36,7 +36,7 @@ const UserKYCWrapper = ({ apiBaseUrl, onNavigate }) => {
 };
 
 function App() {
-  const API_BASE_URL = 'http://localhost:5001'; 
+  const API_BASE_URL = 'https://www.coinora.in'; 
 
   return (
     <HashRouter>
